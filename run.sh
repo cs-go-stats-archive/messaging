e0aab751ccec71b9bb2ec140320288ffdc783110
Viktor Liutianskyi <born.to.run.unix@gmail.com>
2020-01-09 12:45:29 +0200

    Replaced run.sh file to call centralized automation scripts.

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
-docker-compose -p cs-go-stats up --no-recreate -d
+category=infrastructure
+service=messaging
+version=0.2.1
 
-rm -rf `cat folders_to_remove | sed 's/\\r//g'`
+cd ./../../automation_scripts
 
-rm -f ./../../../target/infrastructure/CSGOStats.Infrastructure.Messaging*.nupkg && 
-	dotnet restore -v m ./messaging.sln && 
-	dotnet build -v diag -c Release --no-incremental ./messaging.sln && 
-	dotnet test -v n ./messaging.sln && 
-	dotnet pack -v m -c Release -o ./../../../target/infrastructure/ ./messaging.sln && 
-	dotnet nuget push ./../../../target/infrastructure/CSGOStats.Infrastructure.Messaging*.nupkg -k b8e0f6c7-0f8d-3d80-83dc-eccb59ee6083 --skip-duplicate -n true -t 30 -s http://localhost:8081/repository/nuget-default/
+#<project_context> <repository>
+./verify.sh $category $service
 
-rm -f ./../../../target/infrastructure/CSGOStats.Infrastructure.Messaging*.nupkg
-
-echo ''
-read -p 'Run finished. Pressing any key will terminate this script.'
+#<project_context> <project_name> <package_version> <pack_nuget> <pack_objects> <pack_docker>
+./push.sh $category $service $version yes no no
